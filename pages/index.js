f2e667dc9c61b4c0724e8107904ee12d72665b73
John Brennan <jbrennan@fluentstream.com>
2020-03-19 21:35:31 -0600

    new country selector

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,11 @@
-import useStats from '../utils/useStats';
-
-function Stats(){
-
-    const stats = useStats("https://covid19.mathdro.id/api");
-    if (!stats){return <p>Loading...</p>}
-    console.log(stats)
-    return (
-        <div>
-            <div className="statBlock">
-                <h3>Confirmed:</h3>
-                <span>{stats.confirmed.value}</span>
-            </div>
-            <div className="statBlock">
-                <h3>deaths:</h3>
-                <span>{stats.deaths.value}</span>
-            </div>
-            <div className="statBlock">
-                <h3>recovered:</h3>
-                <span>{stats.recovered.value}</span>
-            </div>
-        </div>
-    );
-}
+import Stats from '../components/Stats';
+import CountrySelector from '../components/CountrySelector';
 
 export default function IndexPage(){
     return (
-        <Stats></Stats>
+        <div>
+            <Stats></Stats>
+            <CountrySelector></CountrySelector>
+        </div>
     )
 }
